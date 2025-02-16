3fbdf26d322df79f0d7faf9535d4629b6d35fe4c
eseegm97 <eseegm97@gmail.com>
2025-02-15 21:44:43 -0600

    week_6_array_methods

--- array_methods/arrays.js
+++ array_methods/arrays.js
@@ -1,6 +1,6 @@
 const steps = ["one", "two", "three"];
-const listTemplate(step) {
-  return
+const listTemplate = (step) => {
+  return `<li>${step}</li>`;
 }
 const stepsHtml = steps.map(listTemplate);
 document.querySelector("#myList").innerHTML = stepsHtml.join();
