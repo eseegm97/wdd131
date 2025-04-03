4434a369946c5293a843a2efd1777550673dc683
eseegm97 <eseegm97@gmail.com>
2025-04-02 20:57:25 -0500

    week_13_project

--- art-page/art-page.js
+++ art-page/art-page.js
@@ -1,8 +1,6 @@
 const carouselContainer = document.querySelector('.carousel-container');
 const notableWorks = document.querySelector('.notable-works');
-
 const images = Array.from(notableWorks.children);
+
 images.forEach(img => {
-    const clone = img.cloneNode(true);
-    notableWorks.appendChild(clone);
-});
+    const
