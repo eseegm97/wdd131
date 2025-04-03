f2a4a7e7bcf8cd9f60d9b8a7c410bb9fb467f741
eseegm97 <eseegm97@gmail.com>
2025-04-02 21:24:06 -0500

    week_13_project

--- art-page/art-page.js
+++ art-page/art-page.js
@@ -1,6 +1,29 @@
-const carouselContainer = document.querySelector('.carousel-container');
 const notableWorks = document.querySelector('.notable-works');
 const images = Array.from(notableWorks.children);
 
 images.forEach(img => {
-    const
+    const clone = img.cloneNode(true);
+    notableWorks.appendChild(clone);
+});
+
+const totalImages = notableWorks.children.length;
+notableWorks.style.width = `${totalImages * 320}px`;
+
+let scrollSpeed = 1;
+let position = 0;
+
+function scrollCarousel() {
+    position -= scrollSpeed; 
+
+    if (position <= -notableWorks.scrollWidth / 2) {
+        position = 0;
+    }
+
+    notableWorks.style.transform = `translateX(${position}px)`;
+    requestAnimationFrame(scrollCarousel);
+}
+
+scrollCarousel();
+
+notableWorks.addEventListener('mouseover', () => scrollSpeed = 0);
+notableWorks.addEventListener('mouseout', () => scrollSpeed = 1);
